521cfd9b44dd2c8cd9a4a8ae0f22e8aea0eba581
A. Guler <arif.glr@hotmail.com>
2016-04-08 21:56:45 +0300

    sadece denen 404 sayfası

---
+++ app/src/main/java/xyz/arifguler/strappv13/_404.java
@@ -0,0 +1,15 @@
+package xyz.arifguler.strappv13;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class _404 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__404);
+//kullanıcı girişi yapılmadığında erişime engell sayfa için geçerli olan sayfa
+    }
+
+}
